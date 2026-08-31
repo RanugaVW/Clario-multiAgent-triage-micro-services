91e6e0483eda2d838ef3557c867e7fb998bc3483
Sineth Wickramaratna <sinethwickramaratna@gmail.com>
2026-08-31 19:46:44 +0530

    Revert "Merge branch 'main' of https://github.com/RanugaVW/Clario-multiAgent-triage-micro-services"
    This reverts commit 61b4589acbb7064a5a2259c44f4f99b20db5972a, reversing
    changes made to 1b47e0c2359590f9ee10ae3c9f8efa3e9b16e84b.

--- services/voice-to-text-service/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-"""Makes the service package importable when pytest runs from this directory."""
-
-import sys
-from pathlib import Path
-
-SERVICE_ROOT = Path(__file__).resolve().parents[1]
-if str(SERVICE_ROOT) not in sys.path:
-    sys.path.insert(0, str(SERVICE_ROOT))
